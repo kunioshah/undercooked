a756b8c3f9fec92af567f4c609639adda074532e
GnarlyLace <159499924+GnarlyLace@users.noreply.github.com>
2024-10-17 18:48:13 -0700

    Update Trial.cs

--- Assets/Scripts/Trial.cs
+++ Assets/Scripts/Trial.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class Trial : MonoBehaviour
 {
+<<<<<<< HEAD
+    // I checked in - Kunal :)))
+    // hi - Dylan
+=======
     // I checked in - Kunal :)))    
+    // The FitnessGram Pacer Test is a multistage aerobic capacity test that progressivel - karson 
+>>>>>>> c9dcc3532d182659eee61a6596b199f682065542
     // Start is called before the first frame update
     void Start()
     {
