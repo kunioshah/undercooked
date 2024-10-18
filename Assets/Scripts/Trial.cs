c9dcc3532d182659eee61a6596b199f682065542
cerupengu <cerupengu@users.noreply.github.com>
2024-10-17 18:43:54 -0700

    made a funny comment

--- Assets/Scripts/Trial.cs
+++ Assets/Scripts/Trial.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Trial : MonoBehaviour
 {
     // I checked in - Kunal :)))    
+    // The FitnessGram Pacer Test is a multistage aerobic capacity test that progressivel - karson 
     // Start is called before the first frame update
     void Start()
     {
