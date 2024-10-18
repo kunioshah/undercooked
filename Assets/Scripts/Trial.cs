67f30c18ddb5c64c159a8df97b0128a1106e87c3
kunioshah <kunioshah@gmail.com>
2024-10-17 18:36:42 -0700

    First commit - Name

---
+++ Assets/Scripts/Trial.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trial : MonoBehaviour
+{
+    // I checked in - Kunal :)))    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
